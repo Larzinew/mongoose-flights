cc8e04c2ad4c0d69857409b85adcb3550dff0788
larzinew <larzinewright@gmail.com>
2023-12-05 15:40:17 -0800

    ticket page

--- routes/tickets.js
+++ routes/tickets.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 // You'll be creating this controller module next
-const ticketsCtrl = require('../controllers/tickets');
+const ticketsCtrl = require('../controllers/flights');
   
 // GET /flights/:id/tickets/new
-router.get('/flights/:id/ticket/new', flightsCtrl.new);
+router.get('/flights/:id/tickets/new', ticketsCtrl.new);
 
 // POST /flights/:id/tickets
 router.post('/flights/:id/ticket', ticketsCtrl.create);
